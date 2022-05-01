3ba460f97b5f1d4ac5e79e2586b1cee8192eadad
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2022-05-01 18:11:02 +0000

    Added Footer component UI and styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,46 @@
-import {Component} from 'react'
+import {
+  FaPinterestSquare,
+  FaInstagram,
+  FaTwitter,
+  FaFacebookSquare,
+} from 'react-icons/fa'
 
 import './index.css'
 
-export default function Footer() {
-  return (
-    <div>
-      <h1>Footer </h1>
+const Footer = () => (
+  <footer className="footer-container">
+    <div className="website-logo-container">
+      <img
+        className="website-logo"
+        src="https://res.cloudinary.com/pavankalyanbandaru/image/upload/v1651083036/tasty-kitchens/footer-website-logo.png"
+        alt="website-footer-logo"
+      />
+      <h1 className="website-title footer-title">Tasty Kitchen</h1>
     </div>
-  )
-}
+
+    <p className="footer-description">
+      The only thing we are serious about is food. <br />
+      Contact us on.
+    </p>
+
+    <div className="social-media-links-container">
+      <div testid="pintrest-social-icon">
+        <FaPinterestSquare className="social-media-link" />
+      </div>
+
+      <div testid="instagram-social-icon">
+        <FaInstagram className="social-media-link" />
+      </div>
+
+      <div testid="twitter-social-icon">
+        <FaTwitter className="social-media-link" />
+      </div>
+
+      <div testid="facebook-social-icon">
+        <FaFacebookSquare className="social-media-link" />
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
