e7cf7e2c42c016566b8755b0812a38f3f35f5b3d
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2022-05-01 18:10:11 +0000

    Modified NotFound component styles

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,32 +1,24 @@
+import {Link} from 'react-router-dom'
 import './index.css'
 
-const NotFound = props => {
-  const redirectToHomepage = () => {
-    const {history} = props
-    history.push('/')
-  }
-
-  return (
-    <div className="not-found-container">
-      <img
-        className="not-found-image"
-        src="https://res.cloudinary.com/pavankalyanbandaru/image/upload/v1651083038/tasty-kitchens/page-not-found.png"
-        alt="not found"
-      />
-      <h1 className="not-found-heading">Page Not Found</h1>
-      <p className="not-found-description">
-        We are sorry, the page you requested could not be found.
-        <br /> Please go back to the homepage
-      </p>
-      <button
-        className="homepage-button"
-        type="button"
-        onClick={redirectToHomepage}
-      >
-        Homepage
+const NotFound = () => (
+  <div className="not-found-container">
+    <img
+      className="not-found-image"
+      src="https://res.cloudinary.com/pavankalyanbandaru/image/upload/v1651083038/tasty-kitchens/page-not-found.png"
+      alt="not found"
+    />
+    <h1 className="not-found-heading">Page Not Found</h1>
+    <p className="not-found-description">
+      We are sorry, the page you requested could not be found.
+      <br className="line-brake" /> Please go back to the homepage.
+    </p>
+    <Link to="/">
+      <button className="homepage-button" type="button">
+        Home Page
       </button>
-    </div>
-  )
-}
+    </Link>
+  </div>
+)
 
 export default NotFound
