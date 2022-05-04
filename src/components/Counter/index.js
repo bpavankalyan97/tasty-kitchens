df2d1d3de2ddeb86bc40eea7eb52f09693d605f8
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2022-05-04 17:22:43 +0000

    Removed redundant code, Modified few styles

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,21 +1,59 @@
-import {Component} from 'react'
-
+import CartContext from '../../context/CartContext'
 import './index.css'
 
-class Counter extends Component {
-  render() {
-    return (
-      <div>
-        <button type="button" onClick={this.onDecrement}>
-          -
-        </button>
-        <div>0</div>
-        <button type="button" onClick={this.onIncrement}>
-          +
-        </button>
-      </div>
-    )
+const Counter = props => {
+  let increaseQuantity = null
+  let decreaseQuantity = null
+  let removeCartItem = null
+
+  const {id, quantity} = props
+
+  const onClickIncrement = () => {
+    increaseQuantity(id)
   }
+
+  const onClickDecrement = () => {
+    const newQuantity = quantity - 1
+    if (newQuantity === 0) {
+      removeCartItem(id)
+    } else {
+      decreaseQuantity(id)
+    }
+  }
+
+  return (
+    <CartContext.Consumer>
+      {value => {
+        increaseQuantity = value.increaseQuantity
+        decreaseQuantity = value.decreaseQuantity
+        removeCartItem = value.removeCartItem
+
+        return (
+          <div className="counter-container">
+            <button
+              className="counter-button"
+              testid="decrement-count"
+              type="button"
+              onClick={onClickDecrement}
+            >
+              -
+            </button>
+            <p className="counter-count" testid="active-count">
+              {quantity}
+            </p>
+            <button
+              className="counter-button"
+              testid="increment-count"
+              type="button"
+              onClick={onClickIncrement}
+            >
+              +
+            </button>
+          </div>
+        )
+      }}
+    </CartContext.Consumer>
+  )
 }
 
 export default Counter
