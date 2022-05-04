b87e6c32913e99bba3e15f346805312a359d513f
Pavan Kalyan Bandaru <bpavankalyan97@gmail.com>
2022-05-04 17:15:06 +0000

    Added Cart context data, functionality in state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Component} from 'react'
 import {Switch, Route, Redirect} from 'react-router-dom'
 
 import Login from './components/Login'
@@ -6,37 +7,125 @@ import Cart from './components/Cart'
 import NotFound from './components/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
 import RestaurantDetails from './components/RestaurantDetails'
+
+import CartContext from './context/CartContext'
 import './App.css'
 
-/*
-const sortByOptions = [
-  {
-    id: 0,
-    displayText: 'Highest',
-    value: 'Highest',
-  },
-  {
-    id: 2,
-    displayText: 'Lowest',
-    value: 'Lowest',
-  },
-]
-*/
+const initializeCartItems = () => {
+  const cartItems = localStorage.getItem('cartData')
 
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={Login} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/cart" component={Cart} />
-    <ProtectedRoute
-      exact
-      path="/restaurant/:id"
-      component={RestaurantDetails}
-    />
+  if (cartItems === null) {
+    return []
+  }
+  return JSON.parse(cartItems)
+}
 
-    <Route exact path="/bad-path" component={NotFound} />
-    <Redirect to="/bad-path" />
-  </Switch>
-)
+class App extends Component {
+  state = {
+    isOrderPlaced: false,
+    cartItems: initializeCartItems(),
+  }
+
+  addCartItem = item => {
+    this.setState(
+      preState => ({
+        isOrderPlaced: false,
+        cartItems: [...preState.cartItems, item],
+      }),
+      this.updateLocalStorage,
+    )
+  }
+
+  getQuantity = id => {
+    const {cartItems} = this.state
+    const currentItem = cartItems.find(item => item.id === id)
+    return currentItem === undefined ? 0 : currentItem.quantity
+  }
+
+  removeCartItem = id => {
+    this.setState(
+      preState => ({
+        cartItems: preState.cartItems.filter(item => item.id !== id),
+      }),
+      this.updateLocalStorage,
+    )
+  }
+
+  increaseQuantity = id => {
+    this.setState(
+      preState => ({
+        cartItems: preState.cartItems.map(item => {
+          if (item.id === id) {
+            return {...item, quantity: item.quantity + 1}
+          }
+          return item
+        }),
+      }),
+      this.updateLocalStorage,
+    )
+  }
+
+  decreaseQuantity = id => {
+    this.setState(
+      preState => ({
+        cartItems: preState.cartItems.map(item => {
+          if (item.id === id) {
+            return {...item, quantity: item.quantity - 1}
+          }
+          return item
+        }),
+      }),
+      this.updateLocalStorage,
+    )
+  }
+
+  placeOrder = () => {
+    this.setState(
+      {
+        cartItems: [],
+        isOrderPlaced: true,
+      },
+      this.updateLocalStorage,
+    )
+  }
+
+  updateLocalStorage = () => {
+    const {cartItems} = this.state
+    localStorage.setItem('cartData', JSON.stringify(cartItems))
+  }
+
+  render() {
+    const {isOrderPlaced, cartItems} = this.state
+
+    const cartContextValue = {
+      cartItems,
+      isOrderPlaced,
+      placeOrder: this.placeOrder,
+      addCartItem: this.addCartItem,
+      getQuantity: this.getQuantity,
+      removeCartItem: this.removeCartItem,
+      increaseQuantity: this.increaseQuantity,
+      decreaseQuantity: this.decreaseQuantity,
+    }
+
+    return (
+      <CartContext.Provider value={cartContextValue}>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <ProtectedRoute
+            exact
+            path="/restaurant/:id"
+            component={RestaurantDetails}
+          />
+
+          <Route exact path="/bad-path" component={NotFound} />
+          <Redirect to="/bad-path" />
+        </Switch>
+      </CartContext.Provider>
+    )
+  }
+}
 
 export default App
